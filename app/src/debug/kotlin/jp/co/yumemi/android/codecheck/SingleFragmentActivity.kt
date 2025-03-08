7b257e152f8b16bdb23709b52f80f47f81010434
Park JongHun <thesn515@gmail.com>
2025-03-08 15:25:13 +0900

    feat: AutoClearedValue取り込み、微修正。

---
+++ app/src/debug/kotlin/jp/co/yumemi/android/codecheck/SingleFragmentActivity.kt
@@ -0,0 +1,43 @@
+package jp.co.yumemi.android.codecheck
+
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
+
+/**
+ * Used for testing fragments inside a fake activity.
+ * ref: https://github.com/android/architecture-components-samples/blob/main/GithubBrowserSample/app/src/main/java/com/android/example/github/util/AutoClearedValue.kt
+ */
+class SingleFragmentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val content = FragmentContainerView(this).apply {
+            layoutParams = FrameLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+            id = androidx.fragment.R.id.fragment_container_view_tag
+        }
+        setContentView(content)
+    }
+
+    fun setFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .add(androidx.fragment.R.id.fragment_container_view_tag, fragment, "TEST")
+            .commit()
+    }
+
+    fun replaceFragment(fragment: Fragment, addToBackStack: Boolean = false) {
+        supportFragmentManager.beginTransaction()
+            .replace(androidx.fragment.R.id.fragment_container_view_tag, fragment)
+            .apply {
+                if (addToBackStack) {
+                    addToBackStack(null)
+                }
+            }
+            .commit()
+    }
+}
