e6c10d6c66257a74924e5d2e9353266808146e90
Park JongHun <thesn515@gmail.com>
2025-03-08 22:26:46 +0900

    fix: kaptって書いてしまったところをkspに修正。 fix: applicationに書こうとしたhiltをlibraryに書いたので修正。

--- build-logic/src/main/kotlin/jp/co/yumemi/android/codecheck/buildlogic/plugin/YumemiApplicationPlujgin.kt
+++ build-logic/src/main/kotlin/jp/co/yumemi/android/codecheck/buildlogic/plugin/YumemiApplicationPlujgin.kt
@@ -26,6 +26,7 @@ class YumemiApplicationPlujgin : Plugin<Project> {
             configureAgpBuildType()
             configureAgpJvmCompatibility()
             configureDetekt()
+            configureHilt()
         }
     }
 }
