367eb8fd52ac4872bfec4816c64ae15e9db1699d
Park JongHun <thesn515@gmail.com>
2025-03-08 15:50:19 +0900

    feat: TopViewModelを使ってlastSearchDate管理。

--- app/src/main/kotlin/jp/co/yumemi/android/codecheck/RepositoryDetailFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/RepositoryDetailFragment.kt
@@ -7,15 +7,18 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
 import coil3.load
-import jp.co.yumemi.android.codecheck.TopActivity.Companion.lastSearchDate
 import jp.co.yumemi.android.codecheck.databinding.FragmentRepositoryDetailBinding
+import kotlinx.coroutines.launch
 
 /**
  * GithubのRepository詳細画面
  */
 class RepositoryDetailFragment : Fragment(R.layout.fragment_repository_detail) {
+    private val topViewModel by activityViewModels<TopViewModel>()
 
     private val args: RepositoryDetailFragmentArgs by navArgs()
 
@@ -23,9 +26,6 @@ class RepositoryDetailFragment : Fragment(R.layout.fragment_repository_detail) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        Log.d("検索した日時", lastSearchDate.toString())
-
         binding = FragmentRepositoryDetailBinding.bind(view)
 
         val item = args.item
@@ -37,5 +37,11 @@ class RepositoryDetailFragment : Fragment(R.layout.fragment_repository_detail) {
         binding.watchersView.text = "${item.watchersCount} watchers";
         binding.forksView.text = "${item.forksCount} forks";
         binding.openIssuesView.text = "${item.openIssuesCount} open issues";
+
+        lifecycleScope.launch {
+            topViewModel.lastSearchDate.collect {
+                Log.d("検索した日時", it.toString())
+            }
+        }
     }
 }
