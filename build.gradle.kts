94d252272e7117c9325c2b22ba504da431a252b4
Park JongHun <thesn515@gmail.com>
2025-03-08 22:11:21 +0900

    feat: hilt追加。

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ plugins {
     alias(libs.plugins.agp.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.navigation) apply false
+    alias(libs.plugins.hilt) apply false
 }
 
 tasks.register("clean", Delete::class) {
