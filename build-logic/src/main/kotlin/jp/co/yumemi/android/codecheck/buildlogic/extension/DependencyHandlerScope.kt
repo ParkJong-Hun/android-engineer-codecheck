b4f25bce48af6bbaeb98d80d2e7b977d076d4258
Park JongHun <thesn515@gmail.com>
2025-03-08 22:11:11 +0900

    feat: DependencyHandlerScopeの拡張関数追加。

---
+++ build-logic/src/main/kotlin/jp/co/yumemi/android/codecheck/buildlogic/extension/DependencyHandlerScope.kt
@@ -0,0 +1,34 @@
+package jp.co.yumemi.android.codecheck.buildlogic.extension
+
+import org.gradle.api.artifacts.MinimalExternalModuleDependency
+import org.gradle.kotlin.dsl.DependencyHandlerScope
+
+fun DependencyHandlerScope.implementation(
+    artifact: MinimalExternalModuleDependency,
+) {
+    add("implementation", artifact)
+}
+
+fun DependencyHandlerScope.debugImplementation(
+    artifact: MinimalExternalModuleDependency,
+) {
+    add("debugImplementation", artifact)
+}
+
+fun DependencyHandlerScope.androidTestImplementation(
+    artifact: MinimalExternalModuleDependency,
+) {
+    add("androidTestImplementation", artifact)
+}
+
+fun DependencyHandlerScope.testImplementation(
+    artifact: MinimalExternalModuleDependency,
+) {
+    add("testImplementation", artifact)
+}
+
+fun DependencyHandlerScope.api(
+    artifact: MinimalExternalModuleDependency,
+) {
+    add("api", artifact)
+}
