5dd0f18b1fcb18664a1752469d0b86dedb978fb1
Park JongHun <thesn515@gmail.com>
2025-03-08 12:05:39 +0900

    feat: classpathをpluginに移行して、libsに作成。

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,18 @@
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
