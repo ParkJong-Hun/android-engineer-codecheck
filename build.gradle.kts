252a61fb72f44258e9ebe8338f4fbdb54cc39be5
Park JongHun <thesn515@gmail.com>
2025-03-08 11:41:58 +0900

    feat: ktsに移行。

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,15 +5,15 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:8.8.0'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.10"
-        classpath "androidx.navigation:navigation-safe-args-gradle-plugin:2.8.8"
+        classpath("com.android.tools.build:gradle:8.8.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.10")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.8.8")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
 }
 
-tasks.register('clean', Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
