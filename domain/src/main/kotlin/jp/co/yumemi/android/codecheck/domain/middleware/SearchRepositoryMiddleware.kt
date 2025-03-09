a31dd0fe89df65bc4ccd4f41f6858476fbd19345
Park JongHun <thesn515@gmail.com>
2025-03-09 19:37:05 +0900

    feat: RepositoryListMiddleware実装。

---
+++ domain/src/main/kotlin/jp/co/yumemi/android/codecheck/domain/middleware/SearchRepositoryMiddleware.kt
@@ -0,0 +1,95 @@
+package jp.co.yumemi.android.codecheck.domain.middleware
+
+import jp.co.yumemi.android.codecheck.domain.entity.SearchedRepositoryItemInfo
+import jp.co.yumemi.android.codecheck.domain.middleware.core.BusinessIntent
+import jp.co.yumemi.android.codecheck.domain.middleware.core.BusinessState
+import jp.co.yumemi.android.codecheck.domain.middleware.core.Middleware
+import jp.co.yumemi.android.codecheck.domain.middleware.core.redux.Reducer
+import jp.co.yumemi.android.codecheck.domain.middleware.core.redux.SideEffectHandler
+import jp.co.yumemi.android.codecheck.domain.middleware.core.redux.Store
+import jp.co.yumemi.android.codecheck.domain.repository.GithubRepository
+
+sealed class SearchRepositoryState : BusinessState {
+    data object None : SearchRepositoryState()
+    data object Loading : SearchRepositoryState()
+    data class Stable(val items: List<SearchedRepositoryItemInfo>) : SearchRepositoryState()
+    data class Error(val message: String) : SearchRepositoryState()
+}
+
+sealed class SearchRepositoryIntent : BusinessIntent {
+    data class Search(val query: String) : SearchRepositoryIntent()
+    internal data class SearchComplete(val searchedItems: List<SearchedRepositoryItemInfo>) :
+        SearchRepositoryIntent()
+
+    internal data class Failure(val throwable: Throwable) : SearchRepositoryIntent()
+}
+
+internal class SearchRepositoryReducer : Reducer<SearchRepositoryState, SearchRepositoryIntent> {
+    override fun reduce(
+        currentState: SearchRepositoryState,
+        intent: SearchRepositoryIntent
+    ): SearchRepositoryState {
+        return when (intent) {
+            is SearchRepositoryIntent.Search -> {
+                SearchRepositoryState.Loading
+            }
+
+            is SearchRepositoryIntent.SearchComplete -> {
+                SearchRepositoryState.Stable(intent.searchedItems)
+            }
+
+            is SearchRepositoryIntent.Failure -> {
+                SearchRepositoryState.Error(intent.throwable.message ?: "Unknown error")
+            }
+        }
+    }
+}
+
+internal class SearchRepositorySideEffectHandler(
+    private val githubRepository: GithubRepository,
+) : SideEffectHandler<SearchRepositoryState, SearchRepositoryIntent> {
+    override suspend fun handle(
+        state: SearchRepositoryState,
+        intent: SearchRepositoryIntent,
+        conveyIntention: (SearchRepositoryIntent) -> Unit
+    ) {
+        when (intent) {
+            is SearchRepositoryIntent.Search -> {
+                when (state) {
+                    is SearchRepositoryState.Loading -> {
+                        runCatching {
+                            githubRepository.getSearchedRepositoryItemInfo(intent.query)
+                        }.onSuccess {
+                            conveyIntention(SearchRepositoryIntent.SearchComplete(it))
+                        }.onFailure {
+                            conveyIntention(SearchRepositoryIntent.Failure(it))
+                        }
+                    }
+
+                    else -> Unit
+                }
+            }
+
+            else -> Unit
+        }
+    }
+}
+
+internal class SearchRepositoryStore(
+    initialState: SearchRepositoryState,
+    reducer: Reducer<SearchRepositoryState, SearchRepositoryIntent>,
+    sideEffectHandlers: List<SideEffectHandler<SearchRepositoryState, SearchRepositoryIntent>> = emptyList()
+) : Store<SearchRepositoryState, SearchRepositoryIntent>(
+    initialState,
+    reducer,
+    sideEffectHandlers
+)
+
+fun searchRepositoryMiddleware(
+    initialState: SearchRepositoryState,
+    githubRepository: GithubRepository,
+): Middleware<SearchRepositoryState, SearchRepositoryIntent> = SearchRepositoryStore(
+    initialState,
+    SearchRepositoryReducer(),
+    listOf(SearchRepositorySideEffectHandler(githubRepository))
+)
--- feature/top/src/main/kotlin/jp/co/yumemi/android/codecheck/feature/top/RepositoryListViewModel.kt
+++ feature/top/src/main/kotlin/jp/co/yumemi/android/codecheck/feature/top/RepositoryListViewModel.kt
@@ -3,7 +3,9 @@ package jp.co.yumemi.android.codecheck.feature.top
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import jp.co.yumemi.android.codecheck.domain.repository.GithubRepository
+import jp.co.yumemi.android.codecheck.domain.middleware.SearchRepositoryIntent
+import jp.co.yumemi.android.codecheck.domain.middleware.SearchRepositoryState
+import jp.co.yumemi.android.codecheck.domain.middleware.core.Middleware
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -14,29 +16,47 @@ import javax.inject.Inject
  */
 @HiltViewModel
 class RepositoryListViewModel @Inject constructor(
-    private val githubRepository: GithubRepository,
+    private val searchRepositoryMiddleware: Middleware<SearchRepositoryState, SearchRepositoryIntent>
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow<RepositoryListUiState>(RepositoryListUiState.None)
     val uiState: StateFlow<RepositoryListUiState> = _uiState
 
-    fun searchRepositories(query: String) {
-        if (query.isEmpty()) return
+    init {
+        collectBusinessState()
+    }
 
+    fun onSearchClick(inputString: String) {
+        if (inputString.isEmpty()) return
         viewModelScope.launch {
-            _uiState.value = RepositoryListUiState.Loading
+            searchRepositoryMiddleware.conveyIntention(SearchRepositoryIntent.Search(inputString))
+        }
+    }
 
-            runCatching {
-                githubRepository.getSearchedRepositoryItemInfo(query)
-            }.onSuccess { results ->
-                if (results.isEmpty()) {
-                    _uiState.value = RepositoryListUiState.Empty
-                } else {
-                    _uiState.value = RepositoryListUiState.Success(results)
+    private fun collectBusinessState() {
+        viewModelScope.launch {
+            searchRepositoryMiddleware.businessState.collect { state ->
+                when (state) {
+                    is SearchRepositoryState.None -> {
+                        _uiState.value = RepositoryListUiState.None
+                    }
+
+                    is SearchRepositoryState.Loading -> {
+                        _uiState.value = RepositoryListUiState.Loading
+                    }
+
+                    is SearchRepositoryState.Stable -> {
+                        if (state.items.isEmpty()) {
+                            _uiState.value = RepositoryListUiState.Empty
+                        } else {
+                            _uiState.value = RepositoryListUiState.Success(state.items)
+                        }
+                    }
+
+                    is SearchRepositoryState.Error -> {
+                        _uiState.value = RepositoryListUiState.Error(state.message)
+                    }
                 }
-            }.onFailure { error ->
-                _uiState.value =
-                    RepositoryListUiState.Error(error.message ?: "Unknown error occurred")
             }
         }
     }
