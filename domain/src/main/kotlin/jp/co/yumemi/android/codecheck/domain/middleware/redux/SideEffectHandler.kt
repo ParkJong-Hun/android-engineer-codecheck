eca7ac31cd5dabb9c340f94fda4455bb6d6367b2
Park JongHun <thesn515@gmail.com>
2025-03-09 18:20:12 +0900

    fix: internalに修正。

--- domain/src/main/kotlin/jp/co/yumemi/android/codecheck/domain/middleware/redux/SideEffectHandler.kt
+++ domain/src/main/kotlin/jp/co/yumemi/android/codecheck/domain/middleware/redux/SideEffectHandler.kt
@@ -3,6 +3,6 @@ package jp.co.yumemi.android.codecheck.domain.middleware.redux
 import jp.co.yumemi.android.codecheck.domain.middleware.BusinessIntent
 import jp.co.yumemi.android.codecheck.domain.middleware.BusinessState
 
-interface SideEffectHandler<S : BusinessState, I : BusinessIntent> {
+internal interface SideEffectHandler<S : BusinessState, I : BusinessIntent> {
     suspend fun handle(state: S, intent: I, conveyIntention: (I) -> Unit)
 }
