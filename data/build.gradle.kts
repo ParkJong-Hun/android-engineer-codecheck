fd2d79933f673ae27f57a5b562480332dbc20114
Park JongHun <thesn515@gmail.com>
2025-03-09 13:24:24 +0900

    feat: data層のためのライブラリー追加。

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -2,14 +2,18 @@ plugins {
     alias(libs.plugins.yumemi.library)
     alias(libs.plugins.hilt)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 android.namespace = "jp.co.yumemi.android.codecheck.data"
 
 dependencies {
     implementation(projects.domain)
+    implementation(libs.kotlinx.serialization.json)
     implementation(libs.ktor.client.core)
     implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.client.content.negotation)
+    implementation(libs.ktor.serialization.json)
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
 }
