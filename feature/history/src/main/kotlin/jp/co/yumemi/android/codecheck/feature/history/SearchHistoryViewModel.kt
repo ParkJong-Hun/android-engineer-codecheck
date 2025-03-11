9c4ce5bde6c4dc63437377e50620d515a6baaa88
Park JongHun <thesn515@gmail.com>
2025-03-11 23:10:16 +0900

    feat: SearchHistoryViewModel、ViewModelTest作成。

---
+++ feature/history/src/main/kotlin/jp/co/yumemi/android/codecheck/feature/history/SearchHistoryViewModel.kt
@@ -0,0 +1,31 @@
+package jp.co.yumemi.android.codecheck.feature.history
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import jp.co.yumemi.android.codecheck.domain.middleware.AppIntent
+import jp.co.yumemi.android.codecheck.domain.middleware.AppState
+import jp.co.yumemi.android.codecheck.domain.middleware.core.Middleware
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchHistoryViewModel @Inject constructor(
+    appStateMiddleware: Middleware<AppState, AppIntent>,
+) : ViewModel() {
+    val uiState = appStateMiddleware.businessState
+        .map {
+            if (it.histories.histories.isNotEmpty()) {
+                SearchHistoryUiState.Idle(it.histories)
+            } else {
+                SearchHistoryUiState.Empty
+            }
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.Eagerly,
+            initialValue = SearchHistoryUiState.Empty,
+        )
+}
