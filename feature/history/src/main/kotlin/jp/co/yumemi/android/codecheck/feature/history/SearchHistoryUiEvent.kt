02cd6197177cf64243d898d486024b5e8db722fc
Park JongHun <thesn515@gmail.com>
2025-03-15 20:11:46 +0900

    feat: SearchHistoryViewModelにUdfViewModelInterface提供。

--- feature/history/src/main/kotlin/jp/co/yumemi/android/codecheck/feature/history/SearchHistoryUiEvent.kt
+++ feature/history/src/main/kotlin/jp/co/yumemi/android/codecheck/feature/history/SearchHistoryUiEvent.kt
@@ -1,7 +1,8 @@
 package jp.co.yumemi.android.codecheck.feature.history
 
 import jp.co.yumemi.android.codecheck.domain.entity.History
+import jp.co.yumemi.android.codecheck.presentation.uiarchitecture.UiEvent
 
-sealed interface SearchHistoryUiEvent {
+sealed interface SearchHistoryUiEvent : UiEvent {
     data class OnClickHistory(val history: History) : SearchHistoryUiEvent
 }
--- feature/history/src/main/kotlin/jp/co/yumemi/android/codecheck/feature/history/SearchHistoryUiState.kt
+++ feature/history/src/main/kotlin/jp/co/yumemi/android/codecheck/feature/history/SearchHistoryUiState.kt
@@ -1,8 +1,9 @@
 package jp.co.yumemi.android.codecheck.feature.history
 
 import jp.co.yumemi.android.codecheck.domain.entity.History
+import jp.co.yumemi.android.codecheck.presentation.uiarchitecture.UiState
 
-sealed interface SearchHistoryUiState {
+sealed interface SearchHistoryUiState : UiState {
     data class Idle(
         val histories: List<History>,
         val onClickedHistory: Pair<History?, Boolean> = null to false,
