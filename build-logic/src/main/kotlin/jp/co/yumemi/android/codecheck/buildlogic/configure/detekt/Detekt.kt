3af8689af753611d32f341d7a0f734bc756fb8f8
Park JongHun <thesn515@gmail.com>
2025-03-16 15:33:23 +0900

    fix: detekt rule依存追加をappモジュール意外にもするように修正。

--- build-logic/src/main/kotlin/jp/co/yumemi/android/codecheck/buildlogic/configure/detekt/Detekt.kt
+++ build-logic/src/main/kotlin/jp/co/yumemi/android/codecheck/buildlogic/configure/detekt/Detekt.kt
@@ -3,7 +3,11 @@ package jp.co.yumemi.android.codecheck.buildlogic.configure.detekt
 import io.gitlab.arturbosch.detekt.Detekt
 import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
 import io.gitlab.arturbosch.detekt.extensions.DetektExtension
+import jp.co.yumemi.android.codecheck.buildlogic.extension.detektPlugins
+import jp.co.yumemi.android.codecheck.buildlogic.extension.library
+import jp.co.yumemi.android.codecheck.buildlogic.extension.libs
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.withType
 
 fun Project.configureDetekt() {
@@ -22,5 +26,10 @@ fun Project.configureDetekt() {
 
     tasks.withType<DetektCreateBaselineTask>().configureEach {
         jvmTarget = "17"
+    }
+
+    dependencies {
+        detektPlugins(libs.library("detekt-compose-rules"))
+        detektPlugins(libs.library("detekt-formatting-rules"))
     }
 }
